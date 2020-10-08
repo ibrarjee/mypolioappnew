2daa846bcd146dd2069292348bf04ab3e3bd2e97
Android <ibrarkhanse@gmail.com>
2020-10-08 04:14:17 +0000

    2nd push

--- app/src/main/java/com/mypolio/mypolioapp/MainActivity.java
+++ app/src/main/java/com/mypolio/mypolioapp/MainActivity.java
@@ -2,18 +2,33 @@ package com.mypolio.mypolioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+  private Button joinNowButton , loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        joinNowButton = (Button) findViewById(R.id.loginbtn);
+
+        loginButton = (Button) findViewById(R.id.join_now);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, login.class);
+                startActivity();
+            }
+        });
+    }
+
+    private void startActivity() {
     }
 }
